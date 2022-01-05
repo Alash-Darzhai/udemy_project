a006a344e5d9b43f4fc64b6b40741f41fbb3f8e6
Alash <aldarzhaj@gmail.com>
2022-01-05 17:05:47 +0200

    Практика 12 часть 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 "use strict";
 
-const isChecked = false,
-      isClose = false;
+const numberOfilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(isChecked || !isClose);
+const personalMovieDB = {
+    count: numberOfilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько его оцените?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = +prompt('На сколько его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
